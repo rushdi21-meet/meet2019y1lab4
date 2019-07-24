78763a3f45d540a163e29684574d999b7509b3e3
rushdi21-meet <rushdi21@meet.mit.edu>
2019-07-24 10:08:12 -0400

    Stopping here

---
+++ part5, Lec4.py
@@ -0,0 +1,11 @@
+import turtle
+def up():
+    turtle.onkeypress(up,"w")
+def down():
+    turtle.onkeypress(down,"s")
+def right():
+    turtle.onkeypress(right,"d")
+def left():
+    turtle.onkeypress(left,"a")
+    turtle.listen()
+turtle.mainloop
